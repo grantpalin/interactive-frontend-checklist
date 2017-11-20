67e6fb58029dde358a72758c4dfe067be4f7173a
Grant Palin <grant@grantpalin.com>
2017-11-20 13:12:14 -0800

    Added tallying of priority levels

--- src/Components/Criteria.js
+++ src/Components/Criteria.js
@@ -5,8 +5,37 @@ import './Criteria.css';
 
 class Criteria extends Component {
   render() {
+    // get an array of child elements (CriteriaGroup) for processing
+    const groups = React.Children.toArray(this.props.children);
+
+    // generate per-group priority tallies
+    const levels = groups.map(group => {
+      const children = group.props.children;
+
+      return {
+        low: children.filter(child => { return child.props.priority === 'low' }).length,
+        med: children.filter(child => { return child.props.priority === 'med' }).length,
+        high: children.filter(child => { return child.props.priority === 'high' }).length
+      };
+    });
+
+    // reduce the per-group priority tallies to a single tally per priority
+    const levelCounts = levels.reduce(function(accumulator, currentValue, currentIndex, array) {
+      return {
+        low: accumulator.low + currentValue.low,
+        med: accumulator.med + currentValue.med,
+        high: accumulator.high + currentValue.high
+      }
+    }, {low: 0, med: 0, high: 0});
+
     return (
       <div className="criteria">
+        <ul className="priority-counts">
+          {levelCounts.high > 0 ? <li className="priority-count high">High: {levelCounts.high}</li> : null }
+          {levelCounts.med > 0 ? <li className="priority-count medium">Medium: {levelCounts.med}</li> : null }
+          {levelCounts.low > 0 ? <li className="priority-count low">Low: {levelCounts.low}</li> : null }
+          <li className="priority-count total">Total: {levelCounts.high + levelCounts.med + levelCounts.low}</li>
+        </ul>
         {this.props.children}
       </div>
     );
--- src/Components/CriteriaGroup.js
+++ src/Components/CriteriaGroup.js
@@ -5,9 +5,25 @@ import './CriteriaGroup.css';
 
 class CriteriaGroup extends Component {
   render() {
+    // get an array of child elements (Criterion) for processing
+    const children = React.Children.toArray(this.props.children);
+
+    // tally the number of each priority level used in this group
+    const levelCounts = {
+      low: children.filter(child => child.props.priority === 'low').length,
+      med: children.filter(child => child.props.priority === 'med').length,
+      high: children.filter(child => child.props.priority === 'high').length,
+    };
+
     return (
       <div className="criteria-group">
         <h2 className="criteria-group-title">{this.props.title}</h2>
+        <ul className="priority-counts">
+          {levelCounts.high > 0 ? <li className="priority-count high">High: {levelCounts.high}</li> : null }
+          {levelCounts.med > 0 ? <li className="priority-count medium">Medium: {levelCounts.med}</li> : null }
+          {levelCounts.low > 0 ? <li className="priority-count low">Low: {levelCounts.low}</li> : null }
+          <li className="priority-count total">Total: {levelCounts.high + levelCounts.med + levelCounts.low}</li>
+        </ul>
         {this.props.children}
       </div>
     );
