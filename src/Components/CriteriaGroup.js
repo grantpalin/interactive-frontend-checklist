6b09214a523af5cb5fe617e743c8adaf5d73937a
Grant Palin <grant@grantpalin.com>
2017-11-25 22:52:49 -0800

    Want to always display the placeholders

--- src/Components/CriteriaGroup.js
+++ src/Components/CriteriaGroup.js
@@ -38,9 +38,9 @@ class CriteriaGroup extends Component {
       <div className="criteria-group">
         <h2 className="criteria-group-title">{this.props.title}</h2>
         <ul className="priority-counts">
-          {this.state.high > 0 ? <li className="priority-count high">High: {this.state.high}</li> : null }
-          {this.state.med > 0 ? <li className="priority-count medium">Medium: {this.state.med}</li> : null }
-          {this.state.low > 0 ? <li className="priority-count low">Low: {this.state.low}</li> : null }
+          <li className="priority-count high">High: {this.state.high}</li>
+          <li className="priority-count medium">Medium: {this.state.med}</li>
+          <li className="priority-count low">Low: {this.state.low}</li>
           <li className="priority-count total">Total: {this.state.high + this.state.med + this.state.low}</li>
         </ul>
         {criteria}
